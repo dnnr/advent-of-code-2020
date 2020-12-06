4b7f1710feb20c4f669087d786c9a207489ce999
Daniel Danner <Daniel.Danner@methodpark.de>
2020-12-06 12:27:48 +0100

    Add solution for part2, day06 (built with yogan)

--- src/day06.rs
+++ src/day06.rs
@@ -1,16 +1,25 @@
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub fn part1(inp: String) {
     let groups = read_groups(&inp);
 
-    let answers = groups.iter().map(|group| get_answers_for_group(group));
+    let any_answers = groups.iter().map(|group| get_any_answers_in_group(group));
 
-    let sum: usize = answers.map(|a| a.len()).sum();
+    let sum: usize = any_answers.map(|a| a.len()).sum();
 
-    println!("sum: {:?}", sum);
+    println!("part1: {:?}", sum);
 }
 
-pub fn part2(_inp: String) {}
+pub fn part2(inp: String) {
+    let groups = read_groups(&inp);
+
+    let common_answers = groups.iter().map(|group| get_common_answers_in_group(group));
+
+    let sum: usize = common_answers.map(|a| a.len()).sum();
+
+    println!("part2: {:?}", sum);
+}
 
 fn read_groups(inp: &str) -> Vec<&str> {
     inp.split("\n\n")
@@ -18,7 +27,25 @@ fn read_groups(inp: &str) -> Vec<&str> {
         .collect::<Vec<&str>>()
 }
 
-fn get_answers_for_group(group: &str) -> HashSet<char> {
+fn get_common_answers_in_group(group: &str) -> HashSet<char> {
+    let people = group
+        .split("\n")
+        .filter(|line| line.len() > 0)
+        .map(|line| HashSet::from_iter(line.chars()))
+        .collect::<Vec<HashSet<char>>>();
+
+    if people.is_empty() {
+        return HashSet::new();
+    }
+
+    let mut people_iter = people.iter();
+    let mut common_answers = people_iter.next().unwrap().clone();
+    people_iter.for_each(|answers| common_answers.retain(|x| answers.contains(x)));
+
+    common_answers
+}
+
+fn get_any_answers_in_group(group: &str) -> HashSet<char> {
     group
         .chars()
         .filter(|c| match c {
@@ -34,11 +61,25 @@ mod test {
     use std::iter::FromIterator;
 
     #[test]
-    pub fn get_answers_for_group_sample1() {
+    pub fn get_any_answers_in_group_sample1() {
         let group = "abcx\nabcy\nabcz\n";
         let expected_answers = vec!['a', 'b', 'c', 'x', 'y', 'z'];
 
-        let answers = get_answers_for_group(group);
+        let answers = get_any_answers_in_group(group);
+
+        assert_eq!(
+            answers,
+            HashSet::from_iter(expected_answers.iter().cloned())
+        );
+    }
+
+
+    #[test]
+    pub fn get_common_answers_in_group_sample1() {
+        let group = "abcx\nabcy\nabcz\n";
+        let expected_answers = vec!['a', 'b', 'c'];
+
+        let answers = get_common_answers_in_group(group);
 
         assert_eq!(
             answers,
