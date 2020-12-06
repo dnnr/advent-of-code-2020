b63af0e68bac4215dcd1df2ef38d5f2c1e8752cd
Daniel Danner <Daniel.Danner@methodpark.de>
2020-12-06 11:08:50 +0100

    Add day06 skeleton

---
+++ src/day06.rs
@@ -0,0 +1,11 @@
+pub fn part1(_inp: String) {}
+
+pub fn part2(_inp: String) {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_nothing() {}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 pub fn noop(_inp: String) {}
 
@@ -14,6 +15,7 @@ pub fn get_day(day: u32) -> (DayFn, DayFn) {
         3 => (day03::part1, day03::part2),
         4 => (day04::part1, noop),
         5 => (day05::part1, noop),
+        6 => (day06::part1, noop),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
